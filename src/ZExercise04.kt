b9f37c196a0de5c8bebf25f64af21aef2cad904f
Marcilio11-du <marcilio11du@gmail.com>
2025-07-22 10:48:56 +0100

    Resolução do quarto exercício proposto

---
+++ src/ZExercise04.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val temps: Array<Float> = arrayOf(23.5f, -2.0f, 17.3f, 42.1f, 8.5f)
+    print(convertToFarenheit(temps).contentToString())
+}
+fun convertToFarenheit(temperatures: Array<Float>): Array<Float> {
+    var convertedTemp: Float
+    for(i in 0..temperatures.size - 1) {
+        val temp = temperatures[i]
+        convertedTemp = (temp * 9/5) + 32
+        temperatures[i] = convertedTemp
+    }
+    return temperatures
+}
